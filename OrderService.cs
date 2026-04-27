8b653f60f3de6d68d60805531ebc903fe21d34a9
Хренюк Д.В. <Daniil.Khrenyuk@yandex.ru>
2026-04-27 16:44:30 +0500

    Убраны поля WareHouse _orderWareHouse и Order _order. Добавлены метод создания заказа и склада которые возвращают ссылку на созданный объект. Созданы списки заказов и складов благодаря которым мы работает в OrderSirvice

--- OrderService.cs
+++ OrderService.cs
@@ -1,62 +1,74 @@
 ﻿using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
 
 namespace OrderInShop
 {
     internal class OrderService
     {
-        private WareHouse _orderWareHouse;
-        private Order _order;
+        private Dictionary<Guid, Order> _orders = new();
+        private Dictionary<Guid, WareHouse> _wareHouses = new();
 
-        // Получается нужно тут делать свой список заказов. Уник ID для заказов. Так как пользователь не всегда регается для заказа.
-        private Guid _orderId;
-        private Dictionary<Guid, Order> OrderDictionary = new();
-
-        public OrderService(WareHouse wareHouse, Order order)
+        public WareHouse CreateNewWareHouse(string wareHouseName)
         {
-            _orderWareHouse = wareHouse;
-            _order = order;
+            var wareH = new WareHouse(wareHouseName);
+            _wareHouses.Add(wareH.Id, wareH);
+            return wareH;
         }
-
-        public void OrderIncreaseQuantity(Item item) => OrderSetQuantity(item, 1); // В UI это "+"
-
-        public void OrderDecreaseQuantity(Item item) => OrderSetQuantity(item, -1); // В UI это "-" //TODO исключение "-1", мб OrderItem использовать
-
-        public void OrderSetQuantity(Item item, int count) // В UI это поле для ввода между "+" и "-" или вызов при выборе товара в каталоге.(сделать два разным метода? в будущем)
+        public Order CreateNewOrder(Guid wareHouseID)
         {
-            if (!_order.HasProduct(item)) // Проверка на наличие товара в корзине
+            var order = new Order(wareHouseID);
+            _orders.Add(order.Id, order);
+            return order;
+        }
+        
+        //public void OrderIncreaseQuantity(Item item) => OrderSetQuantity(item, 1); // В UI это "+"
+
+        //public void OrderDecreaseQuantity(Item item) => OrderSetQuantity(item, -1); // В UI это "-" //TODO исключение "-1", мб OrderItem использовать
+
+        public void SetItemQuantity(Order order, Item item, int count) // В UI это поле для ввода между "+" и "-" или вызов при выборе товара в каталоге.(сделать два разным метода? в будущем)
+        {
+            var wareHouse = GetWareHouseByOrder(order);
+
+            if (!order.HasItem(item)) // Проверка на наличие товара в корзине
             {
-                if (!_orderWareHouse.HasEnoughProduct(item, count)) // Проверка на наличие достаточного кол-ва на складе. 
+                if (!wareHouse.HasEnoughItem(item, count)) // Проверка на наличие достаточного кол-ва на складе. 
                     throw new ArgumentException("Недостаточно товара на складе"); //TODO выдача кол-ва которое есть на складе, вместо Exception
 
-                _order.AddProduct(item, count);
+                order.AddItem(item, count);
             }
             else  // Товар есть в корзине
             {
-                if (!_orderWareHouse.HasEnoughProduct(item, count)) // Проверка на наличие достаточного кол-ва на складе. 
+                if (!wareHouse.HasEnoughItem(item, count)) // Проверка на наличие достаточного кол-ва на складе. 
                     throw new ArgumentException("Недостаточно товара на складе"); //TODO выдача кол-ва которое есть на складе, вместо Exception
 
-                _order.SetCount(item, count);
+                order.SetCount(item, count);
             }
         }
 
         // Оплата.
-        public void OrderPay()
+        public void OrderPay(Order order)
         {
-            _order.OrderList(out var OrderList); // Делаем копию списка корзины.
+            var wareHouse = GetWareHouseByOrder(order);
+
+            order.GetOrderList(out var OrderList); // Делаем копию списка корзины.
 
             foreach (var item in OrderList) // Проверяем, что товара хватает на складе
             {
-                if (!_orderWareHouse.CanReserveProduct(item.Value))
+                if (!wareHouse.CanReserveItem(item.Value))
                     throw new ArgumentException("Недостаточно продуктов на складе");
             }
             
             foreach (var item in OrderList) // Резервирование после проверки, что каждого вида товара хватает на складе. 
             {
-                _orderWareHouse.ReserveProduct(item.Value);
+                wareHouse.ReserveItem(item.Value);
                 // Надо реализовать резервацию на 10 минут.
             }
-
-            _order.Amount();
+        }
+        private WareHouse GetWareHouseByOrder(Order order)
+        {
+            if (!_wareHouses.TryGetValue(order.WareHouseId, out WareHouse wareHouse))
+                throw new Exception("Склада не существует");
+            return wareHouse;
         }
     }
 }
